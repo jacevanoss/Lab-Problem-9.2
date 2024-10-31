c3d9499b6d9b44de9f6c7629a58eee4c713253cc
Jace Van Oss <j-vanoss@onu.edu>
2024-10-31 09:57:48 -0400

    Completed isSortedIncreasing

--- Laboratory_9_P9-2/Laboratory_9_P9-2.cpp
+++ Laboratory_9_P9-2/Laboratory_9_P9-2.cpp
@@ -1,1 +1,42 @@
- 
+/* Date: 10/31/2024
+* Name: Jace Van Oss
+* File: Laboratory_9_P9-2.cpp
+* 
+* Description: program that tests functions for an array of 6 elements
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool isSortedIncreasing(int array[], int size);
+
+int main() {
+
+	const int arraySize = 6;
+	int values[arraySize];
+	for (int i = 0; i < arraySize; i++) {
+		cout << "Enter integer #" << i << ": ";
+		cin >> values[i];
+	}
+	if (isSortedIncreasing(values, arraySize)) {
+		cout << "The data are increasing." << endl;
+	}
+	else {
+		cout << "The data are not increasing." << endl;
+	}
+
+
+
+	return 0;
+}
+bool isSortedIncreasing(int array[], int size) {
+	for (int i = 1; i < size; i++) {
+		if (array[i] < array[i - 1]) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+}
